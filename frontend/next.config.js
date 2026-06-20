6238b6583bc48c94767abaa959d2ca67f540b4ed
Cholif Bima Ardiansyah <74420160+Cholifbima@users.noreply.github.com.>
2026-06-20 18:38:56 +0700

    fix: hapus avif cegah oom

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: false,
-    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       { protocol: 'http', hostname: 'localhost' },
       { protocol: 'http', hostname: '127.0.0.1' },
